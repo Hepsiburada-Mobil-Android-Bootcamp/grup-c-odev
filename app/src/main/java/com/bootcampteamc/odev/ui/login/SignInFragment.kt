d97e3e4c6d6b721c9827777b407e559ad3f0059b
Eda Kuntalp <68751247+edakuntalp@users.noreply.github.com>
2021-10-23 19:56:32 +0300

    added login fragments and activity added font res

---
+++ app/src/main/java/com/bootcampteamc/odev/ui/login/SignInFragment.kt
@@ -0,0 +1,77 @@
+package com.bootcampteamc.odev.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.bootcampteamc.odev.databinding.FragmentSignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInFragment : Fragment() {
+    private val binding: FragmentSignInBinding by lazy { //binding
+        FragmentSignInBinding.inflate(
+            layoutInflater
+        )
+    }
+    private val auth by lazy { FirebaseAuth.getInstance() } //getting instance of FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSignIn.setOnClickListener { //calling sign in function when sign in button clicked
+            signInClick()
+        }
+    }
+
+    fun signInClick() {
+        val userEmail = binding.editTextEmail.text.toString()
+        val password = binding.editTextPassword.text.toString()
+
+        if (userEmail.isNotEmpty() && password.isNotEmpty()) {//check email and password are not empty
+            if (userEmail.contains('@') && userEmail.contains('.') && //check if email has @ and . in it
+                userEmail.length > 5 && userEmail.length < 50                  //and check length
+            ) {
+                binding.editTextEmail.setError(null)
+                binding.textInputLayout.setError(null)
+                auth.signInWithEmailAndPassword(userEmail, password).addOnCompleteListener { task ->
+
+                    if (task.isSuccessful) { //sign in succesful
+                        Toast.makeText(
+                            activity,
+                            "Welcome: ${auth.currentUser?.email.toString()}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        /* val intent = Intent(activity, MainActivity::class.java)
+                         startActivity(intent)
+                         activity?.finish()*/
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(
+                        activity,
+                        exception.localizedMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            } else {
+                binding.editTextEmail.setError("Email is invalid.") //email syntax is invalid, give an error
+            }
+        } else if (userEmail.isEmpty() && password.isNotEmpty()) { //email is empty, give an error
+            binding.editTextEmail.setError("Email is required.")
+        } else if (password.isEmpty()) { //password is empty, give an error
+            binding.textInputLayout.setError("Password is required.")
+        }
+    }
+}
