fa80d0ffd1da3fd4dce0c62d1d478695e7eae0c7
Ozan Takır <ozantakir@gmail.com>
2021-10-24 21:57:40 +0300

    şükürler olsun - mvvm'e taşıma

--- app/src/main/java/com/bootcampteamc/odev/ui/profile/ProfileEditFragment.kt
+++ app/src/main/java/com/bootcampteamc/odev/ui/profile/ProfileEditFragment.kt
@@ -1,46 +1,33 @@
 package com.bootcampteamc.odev.ui.profile
 
-import android.content.Context
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.graphics.drawable.toBitmap
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bootcampteamc.odev.R
-import com.bootcampteamc.odev.data.Product
-import com.bootcampteamc.odev.data.ProfileData
 import com.bootcampteamc.odev.databinding.FragmentProfileEditBinding
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.toObject
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.tasks.await
-import java.io.ByteArrayOutputStream
-import java.util.*
 
 class ProfileEditFragment : Fragment() {
     private lateinit var binding: FragmentProfileEditBinding
     private var firestore: FirebaseFirestore? = null
     private lateinit var imageUri : Uri
+    private var viewModel = ProfileEditViewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileEditBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
 
-        //baya kötü oldu ama ben yaptım -Çağrı
         requireActivity().findViewById<MaterialToolbar>(R.id.toolbar).menu.findItem(R.id.searchFragment).isVisible=false
-        //üstü
+
         firestore = FirebaseFirestore.getInstance()
 
         // upload image
@@ -49,12 +36,13 @@ class ProfileEditFragment : Fragment() {
         ) {
             imageUri = it
             binding.profileImage.setImageURI(it)
+            viewModel.insertUri(it)
         }
         binding.uploadButton.setOnClickListener { getImage.launch("image/*") }
 
         // Save button for saving the info and image
         binding.saveButton.setOnClickListener {
-            save()
+            viewModel.save()
            findNavController().navigate(R.id.homeFragment)
         }
         // Cancel editing profile
@@ -64,87 +52,4 @@ class ProfileEditFragment : Fragment() {
 
         return binding.root
     }
-
-//    // save image with sharedPreferences
-//    private fun saveImage() {
-//        val sharedPref = context?.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
-//        val baos = ByteArrayOutputStream()
-//        val bitmap = binding.profileImage.drawable.toBitmap()
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
-//        val encodedImage = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT)
-//
-//        with(sharedPref!!.edit()) {
-//            putString("encodedImage", encodedImage)
-//            apply()
-//        }
-//
-//    }
-//
-//
-//
-//    // save the written info
-//    private fun saveProfileInfo() {
-//        val sharedPrefer =
-//            activity?.getSharedPreferences("sharedPrefer", Context.MODE_PRIVATE) ?: return
-//        with(sharedPrefer.edit()) {
-//            if (binding.name.text.toString().isNotEmpty()) {
-//                putString("nameInfo", binding.name.text.toString())
-//            }
-//            if (binding.phone.text.toString().isNotEmpty()) {
-//                putString("phoneInfo", binding.phone.text.toString())
-//            }
-//            if (binding.email.text.toString().isNotEmpty()) {
-//                putString("emailInfo", binding.email.text.toString())
-//            }
-//            if (binding.address.text.toString().isNotEmpty()) {
-//                putString("addressInfo", binding.address.text.toString())
-//            }
-//            apply()
-//        }
-//    }
-    private fun save() {
-        val db = FirebaseFirestore.getInstance()
-        val imageAdress = uploadImage(imageUri)
-        val profile = ProfileData(
-            uid = FirebaseAuth.getInstance().currentUser?.uid,
-            name = binding.name.text.toString(),
-            phoneNumber = binding.phone.text.toString(),
-            address = binding.address.text.toString(),
-            imageAddress = imageAdress
-        )
-
-    profile.uid?.let {
-        firestore?.collection("profileInfo")?.document(it)?.set(profile)
-            ?.addOnSuccessListener {
-                Toast.makeText(requireContext(), "Successful", Toast.LENGTH_LONG).show()
-            }?.addOnFailureListener {
-                Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_LONG).show()
-            }
-
-    }
-
-    if (profile.name.isNullOrEmpty() || profile.phoneNumber.toString().isNullOrEmpty()
-        || profile.address.isNullOrEmpty()) {
-        Toast.makeText(requireContext(),"FILL IN THE BLANKS..", Toast.LENGTH_LONG).show()
-    } else {
-        profile.uid?.let {
-            firestore?.collection("profileInfo")?.document(it)?.set(profile)
-                ?.addOnSuccessListener {
-                    Toast.makeText(requireContext(), "Successful", Toast.LENGTH_LONG).show()
-                }?.addOnFailureListener {
-                    Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_LONG).show()
-                }
-
-        }
-    }
-
-    }
-    fun uploadImage(uri: Uri) : String{
-         val ref = FirebaseStorage.getInstance().reference
-        val imageAdress = "images/"+ UUID.randomUUID().toString()
-        ref.child(imageAdress).putFile(uri).addOnSuccessListener {
-        }
-        return imageAdress
-    }
-
 }
--- app/src/main/java/com/bootcampteamc/odev/ui/profile/ProfilePageFragment.kt
+++ app/src/main/java/com/bootcampteamc/odev/ui/profile/ProfilePageFragment.kt
@@ -24,6 +24,7 @@ import com.google.firebase.storage.FirebaseStorage
 class ProfilePageFragment : Fragment() {
     private lateinit var binding: FragmentProfilePageBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private val viewModel = ProfilePageViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +33,8 @@ class ProfilePageFragment : Fragment() {
         binding = FragmentProfilePageBinding.inflate(inflater, container, false)
         requireActivity().findViewById<MaterialToolbar>(R.id.toolbar).menu.findItem(R.id.searchFragment).isVisible=false
         firebaseAuth = FirebaseAuth.getInstance()
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
 
         // to edit profile page
         binding.editButton.setOnClickListener {
@@ -39,65 +42,14 @@ class ProfilePageFragment : Fragment() {
         }
 
         // Loading data
-        getUser()
-        loadImage()
-        loadProfileInfo()
+        viewModel.getUser()
+        viewModel.loadProfileInfo()
 
         return binding.root
     }
-    // if user logged in with google, to get the name and e-mail
-    private fun getUser() {
-        val firebaseUser = firebaseAuth.currentUser
-        if (firebaseUser != null) {
-            val email = firebaseUser.email
-            binding.email.text = email
 
-            val name = firebaseUser.displayName
-            binding.name.text = name
-        }
-    }
     // to upload the image saved
-    private fun loadImage() {
-        val sharedPref = context?.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
-        val encodedImage = sharedPref?.getString("encodedImage", "DEFAULT")
-        if (encodedImage != "DEFAULT") {
-            val imageBytes = Base64.decode(encodedImage, Base64.DEFAULT)
-            val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-            binding.profileImage.setImageBitmap(decodedImage)
-        }
-    }
 
-    // to load the profile info written
-    private fun loadProfileInfo() {
-
-
-        val uid = FirebaseAuth.getInstance().currentUser?.uid
-        val firestore = FirebaseFirestore.getInstance()
-        uid?.let {
-            firestore.collection("profileInfo").document(it).get()
-                .addOnSuccessListener {
-                    val profil = it.toObject(ProfileData::class.java)
-                    binding.apply {
-                        name.text = profil?.name.orEmpty()
-                        phoneNumber.text =profil?.phoneNumber.orEmpty()
-                        address.text = profil?.address.orEmpty()
-                        email.text = firebaseAuth.currentUser?.email.orEmpty()
-                        val ref = FirebaseStorage.getInstance().reference.child(profil?.imageAddress.toString())
-                        ref.downloadUrl.addOnSuccessListener {
-                            Glide.with(binding.root.context).load(it).centerCrop().into(binding.profileImage)
-                        }
-
-                    }
-                    Toast.makeText(requireContext(), "Successful", Toast.LENGTH_LONG).show()
-                }.addOnFailureListener {
-                    Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_LONG).show()
-                }
-
-        }
-
-
-
-    }
 
     override fun onStop() {
         super.onStop()
